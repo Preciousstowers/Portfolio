4a849a34ef60fcf412faba03d9d2fcf838184c27
preciousjane <pjstow@uw.edu>
2021-11-19 11:02:13 -0800

    started coding portfolio

---
+++ my-app/src/App.js
@@ -0,0 +1,56 @@
+
+// import './index.css';
+import React from 'react';
+//import { NavLink } from 'react-router-dom';
+import { Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+   <NavigationBar />
+    </div>
+  );
+}
+
+export function NavigationBar() {
+  return (
+<>
+  <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+  <br />
+  <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+</>
+  )
+}
+
+export default App;
