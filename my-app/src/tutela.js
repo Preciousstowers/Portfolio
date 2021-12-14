e4ac80bbf359afe7ed341510095d6a19e8043491
preciousjane <pjstow@uw.edu>
2021-12-13 18:42:15 -0800

    adding to tutela

--- my-app/src/tutela.js
+++ my-app/src/tutela.js
@@ -21,51 +21,89 @@ export function Tutela() {
             <div className="projectRowOne">
                 <div className="projectOverview">
                     <h2 className="rowTitle">Overview</h2>
-                    <p className="rowDescription">  Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p className="rowDescription">  Tutela is latin for protection and guardianship. </p>
+
+                    <p className="rowDescription"> Our application was inspired by a tiktok trend that included users uploading 
+                        videos of fake video/phone conversations to mimic an active conversation when 
+                        other users have no one to call and are the passanger seat of a car and/or in a potentially 
+                        threatening situation. In the WINFO 9th annual Hackathon, my team and I decided to take 
+                        this trend and design an application because as women-identifying individuals, 
+                        myself and many others are oftentimes seen as potential targets for rape-related 
+                        and Human-trafficking related crimes that continue to persist as higher-institutions lack accountability 
+                        help end these issues. 
+                        </p>
+
+                        <p className="rowDescription">  This application won "Best Overall" in the 9th Annual WINFO Hackathon </p>
+
                 </div>
-                <div className="ProjectDetails">
-                    <h3 className="rowTitle">Project Specifics</h3>
-                    <h5 className="rowTitle">  My Role: Product Designer, Spokespearson  </h5>
-                    <h5 className="rowTitle">  Team size: 4 total people</h5>
-                    <h5 className="rowTitle">  Time Frame: ~30 Hours </h5>
-                    <h5 className="rowTitle">  Tools: Figma, Canva</h5>
-                    <h5 className="rowTitle">  Teammates: </h5>
+                <div className="projectDetails">
+                    <h5 className="rowTitle">Project Specifics</h5>
+                    <p className="rowTitle">  My Role: Product Designer, Spokespearson  </p>
+                    <p className="rowTitle">  Team size: 4 total people</p>
+                    <p className="rowTitle">  Time Frame: ~30 Hours </p>
+                    <p className="rowTitle">  Tools: Figma, Canva</p>
+                    <p className="rowTitle">  Teammates: Kaamna Rishi, Megha Jain and Raveena Rao</p>
                 </div>
             </div>
 
+            <div className="projectRowOne">
+                <section className="leftHandSide">
             <h3 className="rowTitle">Problem</h3>
-            <p className="rowDescriptionLong">In society, human trafficking cases lack equitable data recordings in comparison
-                to other criminal cases, such as burglary. The criminal justice system also lacks
-                equal investigation qualities and motivation when crimes involve female-dominated victims.
-                Additionally, thousands of U.S rape cases are recorded and there has yet to be accountability
-                to the perpetrators and liability to these victims, specifically women.</p>
             <b><p className="rowDescriptionLong">Higher institutions lack the accountability that Human Trafficking and Rape
                 related cases are consistently making women (and others) feel unsafe in society,
                 both in home and in public spaces. </p></b>
-
+            <p className="rowDescriptionLong">
+                <ol>
+                <li>Human trafficking cases lack equitable data recordings in comparison
+                to other criminal cases, such as burglary.</li> 
+                <br/>
+                <li>The criminal justice system also lacks equal investigation qualities and 
+                    motivation when crimes involve female-dominated victims.</li>
+                    <br/>
+                <li>Thousands of U.S rape cases are recorded and there has yet to be accountability
+                to the perpetrators and liability to these victims, specifically women.</li></ol>
+                </p>
+                </section>
+                <section className="leftHandSide">
             <h3 className="rowTitle">Research</h3>
-            <p className="rowDescriptionShort"> Women feel safer in the actual or potential 
+            <p className="rowDescriptionLong"> Women feel safer in the actual or potential 
             presence of others because they assume that offenders will be deterred by the possibility of bystander intervention.
                 - Women's Fear and Design of Public Space</p>
-                <b><p className="rowDescriptionShort">61% of women regularly take steps to avoid being sexually assaulted - YouGov </p></b>
-            <img className="researchPicture"src="tutelaPhotos/YouGov.jpeg" />            <br />
+                <b><p className="rowDescriptionLong">61% of women regularly take steps to avoid being sexually assaulted - YouGov </p></b>
+            <img className="researchPicture"src="tutelaPhotos/YouGov.jpeg" /> 
+            </section>
+            </div>
+             <br />
             <h3 className="rowTitle">Personas</h3>
+            <p className="rowDescriptionLong"> Two user persona's were created to further empathize with women identifying who could potnetially 
+            need bystander intervention. Meet Hannah, a 21 year old college student who does not have a reliable
+            support system and oftentimes find herself walking alone. Meet Jennah, a 25 year old transgender woman
+            who wants to be more self sufficent and finds herself walking alone thoughout her daily errands.</p>
             <div className="projectPersona">
                 <img className="personaOne" src="tutelaPhotos/Hannah's User Persona.jpg" />
                 <img className="personaTwo" src="tutelaPhotos/Jenna's User Persona.png" />
             </div>
+
             <h3 className="rowTitle">Solution</h3>
             <p className="rowDescriptionLong">We wanted to create an application that would offer users a way to connect with family
-                and friends when their safety is compromised. Many women often fear walking or traveling alone,
-                so our app allows users to share their location with desired contacts and play audio or video
+                and friends when they can't connect with anyone and safety is compromised. Many women often fear walking or traveling alone,
+                our app allows users to share their location with desired contacts and play audio or video
                 recordings to provide companionship when users feel alone.</p>
+                 <p className="rowDescriptionLong">
+                 
+                <ol>
+                <li>Reassures users, primarily women-idenifiying that they are never alone </li> 
+                <li>Provides more holistic information for institutions 
+                    when/if a Human-Trafficking case/rape-related case happens</li>
+                <li>Maintains optional in-home security for domestic violence situations 
+                    by implementing Connect to Alexa/Google Home feature </li></ol>
+                  
+                </p>  
+
             <h3 className="rowTitle">Main Features</h3>
+            <img className="initialImageOne" src="/tutelaPhotos/Rectangle-4.png" />
+
+
             <h3 className="rowTitle">Award</h3>
             <img src="tutelaPhotos/WINFO9thHackathonWinningPoster.jpeg" />
         </div>
